dd33f729ef322ad05565dcdd2ae41b450b77824a
Kenya Moore <kenyacmoore@gmail.com>
2021-12-29 08:07:18 -0500

    graph changes

--- src/app/home/graph/graph.component.ts
+++ src/app/home/graph/graph.component.ts
@@ -27,47 +27,53 @@ export class GraphComponent implements OnInit {
   dateRange: string[] = [];
   timeSeries: any = {};
   roundTo: number = 1000*60;
-  yVals: number[] = [];
-  months: any = {
-    'Jan': '01',
-    'Feb': '02',
-    'Mar': '03',
-    'Apr': '04',
-    'May': '05',
-    'Jun': '06',
-    'Jul': '07',
-    'Aug': '08',
-    'Sep': '09',
-    'Oct': '10',
-    'Nov': '11',
-    'Dec': '12'
-  };
-
-  roundToMin = (x: number) => Math.floor(x / this.roundTo) * this.roundTo;
+  axes: any = {};
+  maxY: number = 0;
+  minY: number = Number.MAX_SAFE_INTEGER;
+  line!: string;
 
   constructor(private transactionService: TransactionService,
               private alphaVantageService: AlphaVantageService) { }
 
   ngOnInit(): void {
     this.setSVGDims();
-    this.alphaVantageService.getData('TIME_SERIES_INTRADAY', 'AAPL', '1min').subscribe({
+    this.transactionService.getTransactions().subscribe({
       next: d => {
-        this.timeSeries['AAPL'] = d["Time Series (1min)"];
-        this.dateRange = this.alphaVantageService.dateRange(this.timeSeries['AAPL']);
+        this.transactions = d;
       },
       complete: () => {
-        console.log(this.timeSeries['AAPL']);
-        this.transactionService.getTransactions().subscribe({
+        this.alphaVantageService.getData('TIME_SERIES_INTRADAY', 'AAPL', '1min').subscribe({
           next: d => {
-            this.transactions = d;
+            this.timeSeries['AAPL'] = d["Time Series (1min)"];
           },
           complete: () => {
+            console.log(this.timeSeries['AAPL']);
+            console.log(this.transactions);
             const innerSvgW = this.svgW - this.marginLeft;
+            for(const key in this.timeSeries['AAPL']){
+              for(let i = 0; i < this.transactions.length; i++){
+                if(this.transactions[i].name === 'AAPL'){
+                  let totalProfit = 0;
+                  if(key > this.transactions[i].date.toString()){
+                    totalProfit = this.transactions[i].volume * this.timeSeries['AAPL'][key]['4. close'];
+                    if (key in this.axes){
+                      this.minY = (this.minY > (this.axes[key] + totalProfit))? this.axes[key] + totalProfit: this.minY;
+                      this.maxY = (this.maxY < (this.axes[key] + totalProfit))? this.axes[key] + totalProfit: this.maxY;
+                    }
+                    else {
+                      this.minY = (this.minY > totalProfit)? totalProfit: this.minY;
+                      this.maxY = (this.maxY < totalProfit)? totalProfit: this.maxY;
+                    }
+                    (key in this.axes) ? this.axes[key] += totalProfit : this.axes[key] = totalProfit;                    
+                  }
+                }
+              }
+            }
+            this.drawValue(this.minY, this.maxY, Object.entries(this.axes).sort())
           }
         });
       }
-    });
-    
+    })    
   }
 
   setSVGDims() {
@@ -77,10 +83,17 @@ export class GraphComponent implements OnInit {
     };
   }
 
-  convertDate(date: string) {
-    let str: string[] = date.split(' ');
-    let s = str[3] + '-' + this.months[str[1]] + '-' +str[2] + ' ' + str[4];
-    return s;
+  drawValue(min: number, max: number, axes: any[][]){
+    console.log(min, max);
+    console.log(axes);
+    console.log(axes[0][1])
+    let getH = (y: number) => (this.svgH - this.marginBottom) * ((y - min) / (max - min));
+    let increment = this.svgW / axes.length;
+    console.log(increment);
+    this.line = "M" + this.marginLeft + "," + getH(axes[0][1]);
+    for(let i = 1; i < axes.length; i++){
+      this.line += 'L' + (this.marginLeft + (increment * i)) + ',' + getH(axes[i][1]);
+    }
   }
 
 }
